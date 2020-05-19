db6ad6d2d93ba2372039ca1fd525f75349877148
Nassim-Boutaleb <nassim.boutaleb@edu.ece.fr>
2020-05-19 17:34:22 +0200

    Essai GIT

--- Projet_JAVA_S2/src/Vue/AdminGestionMainPage.java
+++ Projet_JAVA_S2/src/Vue/AdminGestionMainPage.java
@@ -92,10 +92,12 @@ public class AdminGestionMainPage extends JFrame implements ActionListener
         M_barreDeMenus.add(JM_seances);
         M_barreDeMenus.add(JM_cours);
         M_barreDeMenus.add(JM_reporting);
-        M_barreDeMenus.setBackground(Color.white);
+        M_barreDeMenus.setBackground(Color.blue);
         
         // Ajouter la barre de menu dans la partie nord du panel global
         panelGlobal.add(M_barreDeMenus, BorderLayout.NORTH);
+        
+        
         
     }
     
